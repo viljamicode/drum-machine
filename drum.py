823f53829a1c28015603d678b4bfec5e80cbd6a0
viljami <119853988+viljamicode@users.noreply.github.com>
2023-11-15 08:38:55 +0200

    Update drum.py
    Changed this into a smarter and more functional version 5 months ago, but did not update to Github.

--- drum.py
+++ drum.py
@@ -1,42 +1,65 @@
-import tkinter as tk
-from tkinter import *
 import pygame
+import math
 
-# Initialize Pygame and the mixer
-pygame.init()
 pygame.mixer.init()
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Create the name for the game window
+pygame.display.set_caption("Viljami's Mega Cool Drum Machine v 1.337")
 
 # Load the sounds
 kick = pygame.mixer.Sound('kick.wav')
 snare = pygame.mixer.Sound('snare.wav')
 hihat = pygame.mixer.Sound('hihat.wav')
 
-# Create the GUI
-root = tk.Tk()
-root.title('Drum Machine')
-
-
-def key_pressed(event):             # Handle keyboard events
-    if event.char == 'h':           # Handle keyboard events
-        hihat.play()                # Handle keyboard events
-    elif event.char == 'k':         # Handle keyboard events
-        kick.play()                 # Handle keyboard events
-    elif event.char == 's':         # Handle keyboard events
-        snare.play()                # Handle keyboard events
-
-
-root.bind('<Key>', key_pressed)     # Handle keyboard events
-
 # Load the images
-kick_img = tk.PhotoImage(file='kick.png')
-snare_img = tk.PhotoImage(file='snare.png')
-hihat_img = tk.PhotoImage(file='hihat.png')
+snare_image = pygame.image.load('snare.png')
+hihat_image = pygame.image.load('hihat.png')
+kick_image = pygame.image.load('kick.png')
 
-# Create the buttons
-tk.Button(root, image=snare_img,
-          command=lambda: snare.play()).pack(side=tk.LEFT)
-tk.Button(root, image=hihat_img, command=lambda: hihat.play()).pack()
-tk.Button(root, image=kick_img, command=lambda: kick.play()).pack(side=tk.RIGHT)
+# Scale the images
+image_size = (150, 150)
+snare_image = pygame.transform.scale(snare_image, image_size)
+hihat_image = pygame.transform.scale(hihat_image, image_size)
+kick_image = pygame.transform.scale(kick_image, image_size)
 
-# Start the GUI event loop
-root.mainloop()
+# Drum pos
+snare_pos = (WIDTH // 4 - image_size[0], HEIGHT // 2 - image_size[1] // 2)
+hihat_pos = (WIDTH // 2 - image_size[0] // 2, HEIGHT // 2 - image_size[1] // 2)
+kick_pos = (WIDTH // 4 * 3, HEIGHT // 2 - image_size[1] // 2)
+
+
+# Display the drums
+def draw():
+    screen.fill((255, 255, 255))
+    screen.blit(snare_image, snare_pos)
+    screen.blit(hihat_image, hihat_pos)
+    screen.blit(kick_image, kick_pos)
+    pygame.display.flip()
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_h or event.key == pygame.K_u or event.key == pygame.K_i:
+                hihat.play()
+            elif event.key == pygame.K_k or event.key == pygame.K_o or event.key == pygame.K_m:
+                kick.play()
+            elif event.key == pygame.K_s or event.key == pygame.K_a or event.key == pygame.K_d:
+                snare.play()
+    
+    draw()
+
+# Default key binds:
+# Default kick = K,O,M
+# Default hihat = H,U,I
+# Default snare = S,A,D
+
+pygame.quit()
